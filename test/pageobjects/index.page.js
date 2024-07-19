62d31a59b12e16139ad0d71d38a50b8a32c0e39e
seethamchokkapu <116893886+seethamchokkapu@users.noreply.github.com>
2024-07-19 10:35:28 +0530

    removed unwanted files

--- test/pageobjects/index.page.js
+++
@@ -1,7 +0,0 @@
-const { $, driver } = require('@wdio/globals')
-const Page = require('./page');
-const LoginPage = require("./bookingLogin.page");
-const OneWayPage = require("./roundTripFlights.page");
-
-module.exports = {LoginPage,OneWayPage};
-
--- utils/util.js
+++
@@ -1,4 +0,0 @@
-
-module.exports=function platform(){
-    return driver.isAndroid ? 'android' : 'ios';
-}
